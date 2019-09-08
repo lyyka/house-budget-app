cfa90fc539b2cb8e518c5ced05fb720fc9799575
Luka Robajac <luka.robajac@gmail.com>
2019-09-08 21:09:32 +0200

    Gates and expense policies applied to Excel functions

--- resources/views/components/dashboard/households/money.blade.php
+++ resources/views/components/dashboard/households/money.blade.php
@@ -24,14 +24,16 @@
         <p class="mb-1 text-muted">
             Budget will be reset on {{ date('d/m/Y', strtotime('+1 month', strtotime(date("Y") . '-' . date('m') . '-' . $household->budget_reset_day))) }}
         </p>
-        <hr />
     @endcan
-    <button type="button" class="mb-2 py-1 px-3 bg-info text-white rounded shadow-sm border" data-toggle="modal" data-target="#exportDataToXlsxModal">
-        <i class="fas fa-file-export"></i>
-        Export to Excel
-    </button>
-    <button type="button" class="mb-2 py-1 px-3 bg-success text-white rounded shadow-sm border" data-toggle="modal" data-target="#importDataFromXlsxModal">
-        <i class="fas fa-file-upload"></i>
-        Import from Excel
-    </button>
+    @can('view-expense', $household)
+        <hr />
+        <button type="button" class="mb-2 py-1 px-3 bg-info text-white rounded shadow-sm border" data-toggle="modal" data-target="#exportDataToXlsxModal">
+            <i class="fas fa-file-export"></i>
+            Export to Excel
+        </button>
+        <button type="button" class="mb-2 py-1 px-3 bg-success text-white rounded shadow-sm border" data-toggle="modal" data-target="#importDataFromXlsxModal">
+            <i class="fas fa-file-upload"></i>
+            Import from Excel
+        </button>
+    @endcan
 </div>
